a9fac4b8070a6d104a303e2f750466bf4fba703a
wwaliul <waliul.i@outlook.com>
2024-02-17 13:57:23 +0000

    further changes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import {
     getDatabase, 
     ref, 
     push, 
-    onValue } 
+    onValue,
+    update } 
     from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = { 
@@ -37,11 +38,19 @@ btnEl.addEventListener("click", function(){
     const fromValue = fromInputFieldEl.value
     const toValue = toInputFieldEl.value
 
-    let arr = [inputValue, fromValue, toValue]
-    
-    pushToDb(arr)
+    if (inputValue && fromValue && toValue) {
+        let arr = [inputValue, fromValue, toValue, 0]
+        pushToDb(arr)
 
-    clearFieldEl()
+        clearFieldEl()
+
+    } else {
+        inputFieldEl.style.borderColor = "red"
+        fromInputFieldEl.style.borderColor = "red"
+        toInputFieldEl.style.borderColor = "red"
+
+        alert("Fill in the damn thing")
+    }
 })
 
 function pushToDb(item){
@@ -54,39 +63,53 @@ function pushToList(value) {
     let postText = postData[0]
     let postFrom = postData[1]
     let postTo = postData[2]
+    let likeData = postData[3]
 
+    console.log(likeData)
+
+    // dynamic creation of HTML elements
     let newListEl = document.createElement("li")
-    let newListId = newListEl.setAttribute("id", "post-cards")
-
     let mainDivEl = document.createElement("div")
     let toEl = document.createElement("h3")
     let textEl = document.createElement("p")
+    let likeBtnEl = document.createElement("button")
+    let likeCountEl = document.createElement("p")
     let fromEl = document.createElement("h3")
 
+    // tagging id to the respective elements using .setAttribute - "id"
+    let newListId = newListEl.setAttribute("id", "post-cards")
+    let likeBtnId = likeBtnEl.setAttribute("id", "like-btn")
+    
+    // Assigning value to elements
     fromEl.textContent = `From ${postFrom}`
     toEl.textContent = `To ${postTo}`
     textEl.textContent = postText
+    likeBtnEl.textContent = "Like"
+    likeCountEl.textContent = likeData
 
+    // Appending / Nesting Elements
     newListEl.appendChild(mainDivEl)
     mainDivEl.appendChild(toEl)
     mainDivEl.appendChild(textEl)
+    mainDivEl.appendChild(likeBtnEl)
     mainDivEl.appendChild(fromEl)
 
     listEl.append(newListEl)
 
+
+    likeBtnEl.addEventListener("click", function(){
+        likeData += 1 
+        let exactLocationInDb = ref(database, `posts/${postId}`)
+        update(exactLocationInDb, {
+            3: likeData,
+        })
+        console.log(likeData)
+
+    })
     console.log(newListEl)
 
-    // listEl.innerHTML += `<li>
-    // <div>
-    // To ${postTo}
-    // <br>
-    // ${postText}
-    // <br>
-    // From ${postFrom}
-    // </div>
-    // <button>Like</button>
-    // </li>`
 }
+
 
 function clearListEl(){
     listEl.innerHTML = ""
